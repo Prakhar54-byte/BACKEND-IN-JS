19bf085354195a74cb908e15a894fc7a197821fa
{new_username} <b23bb1032@iitj.ac.in>
2024-09-27 18:47:49 +0530

    multer is added as middelware

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {// cb callback function
+      cb(null, "./public/temp") // null means no error
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname) // change it to a unique name
+    }
+  })
+  
+ export  const upload = multer({ 
+    storage: storage 
+})
