1b94dfb0929ca9064fd2d7fc88274e47300e6af1
user.email <b23bb1032@iitj.ac.in>
2024-12-04 18:20:20 +0530

    multer and cloudinary done

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -6,10 +6,13 @@ const storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
       
-      cb(null, file.originalname) // change it to a unique name
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix) // change it to a unique name
     }
   })
   
 export  const upload = multer({ 
     storage: storage 
 })
+
+
