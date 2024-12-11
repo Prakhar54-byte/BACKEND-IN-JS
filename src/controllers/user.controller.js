bb39240ecd2598ff4d3528c9469b6c0992b2dea4
user.email <b23bb1032@iitj.ac.in>
2024-12-12 00:24:48 +0530

    add error handling for getUserChannelProfile: return 404 if channel not found

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -416,6 +416,16 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     
   ]);
   console.log("This is channel",channel);
+
+  if(!channel?.length){
+    throw new ApiError(404,"Channel not found")
+  }
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200,channel[0],"Channel found")
+  )
 });
 getUserChannelProfile()
 
