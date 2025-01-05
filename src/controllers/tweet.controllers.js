2cba2d9d943b4f9f6dbab9cf73a1ca4cc0987835
Prakhar54-byte <b23bb1032@iitj.ac.in>
2025-01-05 21:10:31 +0530

    just format

--- src/controllers/tweet.controllers.js
+++ src/controllers/tweet.controllers.js
@@ -1,156 +1,114 @@
-import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Tweet } from "../models/tweet.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
-    const { content} =req.User
+  //TODO: create tweet
+  const { content } = req.User;
 
-    if(!content){
-        throw new ApiError(
-            400,
-            "Their is nothing to tweet"
-        )
-    }
-    const tweet = await Tweet.create({
-        content,
-        owner: req.user._id
+  if (!content) {
+    throw new ApiError(400, "Their is nothing to tweet");
+  }
+  const tweet = await Tweet.create({
+    content,
+    owner: req.user._id,
+  });
 
-    })
-
-    return res
+  return res
     .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            tweet,
-            "Tweet created successfully"
-        )
-    )
-
-})
+    .json(new ApiResponse(200, tweet, "Tweet created successfully"));
+});
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
-    const { userId } = req.params
+  // TODO: get user tweets
+  const { userId } = req.params;
 
-    if(!userId){
-        throw new ApiError(400,"User not found")
-    }
-    const tweet = await Tweet.aggregate([
-        { 
-            $match : {
-                owner ; mongoose.Types.ObjectId(userId)
-            }
-        },
-        {
-            $lookup:{
-                from:"users",
-                localField:"owner",
-                foreignField:"_id",
-                as: "ownerDetails"
+  if (!userId) {
+    throw new ApiError(400, "User not found");
+  }
+  const tweet = await Tweet.aggregate([
+    {
+      $match: {
+        owner: mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "ownerDetails",
+      },
+    },
+    { $unwind: "$ownerDetails" },
+    {
+      $project: {
+        content: 1,
+        createdAt: 1,
+        updatedAt: 1,
+        "ownerDetails.username": 1,
+        "ownerDetails.avatar": 1,
+      },
+    },
+  ]);
 
-            }
-        },
-        { $unwind: "$ownerDetails" },
-        {
-            $project:{
-                content: 1,
-                createdAt: 1,
-                updatedAt: 1,
-                "ownerDetails.username": 1,
-                "ownerDetails.avatar": 1,
-            }
-        }
-
-    ])
-
-    return res 
+  return res
     .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            tweet,
-            "User tweets fetched successfully"
-        )
-    )
-
-})
+    .json(new ApiResponse(200, tweet, "User tweets fetched successfully"));
+});
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
-    const { content} = req.body
-    const { tweetId } = req.params
+  //TODO: update tweet
+  const { content } = req.body;
+  const { tweetId } = req.params;
 
-    if(!tweetId){
-        throw new ApiError(400,"Invalid tweet ID")
+  if (!tweetId) {
+    throw new ApiError(400, "Invalid tweet ID");
+  }
+  if (!content || content.trim() === "") {
+    throw new ApiError(400, "Content cannot be empty");
+  }
+  const tweet = await Tweet.findByIdAndUpdate(
+    {
+      _id: tweetId,
+      owner: req.user._id,
+    },
+    {
+      $set: { content },
+    },
+    {
+      new: true,
     }
-    if(!content || content.trim() ===""){
-        throw new ApiError(400,"Content cannot be empty")
-    }
-    const tweet = await Tweet.findByIdAndUpdate(
-        {
-            _id:tweetId,
-            owner:req.user._id
+  );
+  if (!tweet) {
+    throw new ApiError(400, "Tweet not found or you are not owner");
+  }
 
-        },
-        {
-            $set:{content}
-        },
-        {
-            new:true
-        }
-    )
-    if(!tweet){
-        throw new ApiError(400,"Tweet not found or you are not owner")
-    }
-
-    return res 
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            tweet,
-            "Tweet added"
-        )
-    )
-})
+  return res.status(200).json(new ApiResponse(200, tweet, "Tweet added"));
+});
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
-    const { twwetId } = req.params
-    if(!isValidObjectId(twwetId)){
-        throw new ApiError(400,"Tweet ID is not found ")
-    }
+  //TODO: delete tweet
+  const { twwetId } = req.params;
+  if (!isValidObjectId(twwetId)) {
+    throw new ApiError(400, "Tweet ID is not found ");
+  }
 
-    const tweet = await Tweet.findByIdAndDelete(
-        {
-            _id: tweetId,
-            owner:req.user._id
-        }
-    )
+  const tweet = await Tweet.findByIdAndDelete({
+    _id: tweetId,
+    owner: req.user._id,
+  });
 
-    if(!tweet){
-        throw new ApiError(400,"Tweet not found or you're not the owner")
-    }
+  if (!tweet) {
+    throw new ApiError(400, "Tweet not found or you're not the owner");
+  }
 
-    return res
+  return res
     .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            {},
-            "Tweet deleted successfully"
-        )
-    )
-})
+    .json(new ApiResponse(200, {}, "Tweet deleted successfully"));
+});
 
-export {
-    createTweet,
-    getUserTweets,
-    updateTweet,
-    deleteTweet
-}
+export { createTweet, getUserTweets, updateTweet, deleteTweet };
