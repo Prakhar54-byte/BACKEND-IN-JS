831028fb7faca9d7d4fc3c3648d87deb26068561
Prakhar54-byte <b23bb1032@iitj.ac.in>
2025-01-08 23:04:04 +0530

    my logic added on toggleVideoLike

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -7,6 +7,40 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
+    if(!videoId || mongoose.Types.isValidObjectId(videoId)){
+        throw new ApiError(400,"Video ID does not exit to toggle like on video")
+    }
+
+    const existingLike = await Like.findOne({
+        video:videoId,
+        likeBy:req.user._id
+    })
+let liked 
+    if(existingLike){
+        await existingLike.remove()
+        liked = false
+    }else{
+        const newLike = await Like.create(
+            {
+                vide:videoId,
+                likeBy:req.user._id
+            }
+        )
+        liked = true
+    }
+
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            liked,
+            "Like toggled successfully by user "
+        )
+    )
+
+    
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
