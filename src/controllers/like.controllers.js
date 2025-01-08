efdf06ae6111ba3b523b5b3d407778cbbc6e761e
Prakhar54-byte <b23bb1032@iitj.ac.in>
2025-01-08 23:08:47 +0530

    add try and catch for error handling

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -5,40 +5,44 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
-    if(!videoId || !mongoose.Types.isValidObjectId(videoId)){
-        throw new ApiError(400,"Video ID does not exit to toggle like on video")
-    }
-
-    const existingLike = await Like.findOne({
-        video:videoId,
-        likedBy:req.user._id
-    })
-let liked 
-    if(existingLike){
-        await existingLike.remove()
-        liked = false
-    }else{
-        const newLike = await Like.create(
-            {
-                vide:videoId,
-                likedBy:req.user._id
-            }
-        )
-        liked = true
-    }
-
-
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            {liked,videoId},
-            "Like toggled successfully by user "
-        )
-    )
+   try {
+     const {videoId} = req.params
+     //TODO: toggle like on video
+     if(!videoId || !mongoose.Types.isValidObjectId(videoId)){
+         throw new ApiError(400,"Video ID does not exit to toggle like on video")
+     }
+ 
+     const existingLike = await Like.findOne({
+         video:videoId,
+         likedBy:req.user._id
+     })
+ let liked 
+     if(existingLike){
+         await existingLike.remove()
+         liked = false
+     }else{
+         const newLike = await Like.create(
+             {
+                 vide:videoId,
+                 likedBy:req.user._id
+             }
+         )
+         liked = true
+     }
+ 
+ 
+     return res
+     .status(200)
+     .json(
+         new ApiResponse(
+             200,
+             {liked,videoId},
+             "Like toggled successfully by user "
+         )
+     )
+   } catch (error) {
+    throw new ApiError(400,error?.message || "Some error in toggleVideoLike")
+   }
 
     
 })
