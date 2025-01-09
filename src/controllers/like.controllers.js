705f4d2e6fc0bd4ab54a2b362b5ec7df879f4834
Prakhar54-byte <b23bb1032@iitj.ac.in>
2025-01-09 19:25:04 +0530

    toggleTweetLike is done

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -86,10 +86,53 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
-    //TODO: toggle like on tweet
-}
-)
+  try {
+      const {tweetId} = req.params
+      //TODO: toggle like on tweet
+      if(!tweetId || !mongoose.Types.isValidObjectId(tweetId)){
+          throw new ApiError(400,"Tweet ID is does not exist to like tweet")
+      }
+  let liked;
+      const existingLike = await Like.findOne({
+          tweet:tweetId,
+          likedBy:req.user._id
+      },
+  )
+  
+      if(existingLike){
+          await existingLike.remove();
+          liked = false
+      }else{
+          const newLike = await Like.create(
+              {
+                  tweet:tweetId,
+                  likedBy:req.user._id
+              },
+              liked = true
+          )
+      }
+  
+      return res
+      .status(200)
+      .json(
+          new ApiResponse(
+              200,
+              {tweetId,likedBy},
+              "Like toggled in tweet by user successfully"
+          )
+      )
+  
+  
+  }
+  
+  catch (error) {
+    throw new ApiError(400,error?.message || "Some error in toggleTweetLike")
+  }
+
+})
+
+
+
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
