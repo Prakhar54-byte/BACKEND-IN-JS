54f11155d9d2053e5f88bc7a1cc8fd2c7be45734
Prakhar54-byte <b23bb1032@iitj.ac.in>
2025-01-13 21:57:27 +0530

    Add validation and pagination for fetching video comments

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -8,6 +8,43 @@ const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
+    if(!videoId || !mongoose.isValidObjectId(videoId)){
+        throw new ApiError(400, "Video Id is incorrect to get all comments")
+    }
+
+    const comments = await Comment.aggregatePaginate(
+        
+        {
+            $match: {
+                video: mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            page,
+            limit
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner"
+            }
+        },
+        {
+            $unwind: "$owner"
+        },
+        {
+            $project: {
+                "owner.password": 0,
+                "owner.email": 0
+            }
+        },
+        Comment.find({video: videoId}),
+        {page, limit}
+    )
+
+    return res.status(200).json(new ApiResponse(200, {videoId, comments}, "All comments get"))
 
 })
 
