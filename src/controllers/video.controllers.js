5c5c1fc42597c187df66ea7bf52585b9668eebe6
Prakhar54-byte <b23bb1032@iitj.ac.in>
2025-01-13 17:44:46 +0530

    Remove unnecessary whitespace in video controller functions

--- src/controllers/video.controllers.js
+++ src/controllers/video.controllers.js
@@ -12,6 +12,7 @@ const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
     
+    
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
@@ -132,6 +133,7 @@ const getVideoById = asyncHandler(async (req, res) => {
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+
 
 })
 
