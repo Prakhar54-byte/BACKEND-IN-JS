194b7f0f67c0026fa0a7ab54b486a37793af36d0
Prakhar54-byte <b23bb1032@iitj.ac.in>
2025-01-13 21:58:01 +0530

    Add error handling to getVideoComments controller

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -5,46 +5,51 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
-    if(!videoId || !mongoose.isValidObjectId(videoId)){
-        throw new ApiError(400, "Video Id is incorrect to get all comments")
-    }
-
-    const comments = await Comment.aggregatePaginate(
-        
-        {
-            $match: {
-                video: mongoose.Types.ObjectId(videoId)
-            }
-        },
-        {
-            page,
-            limit
-        },
-        {
-            $lookup: {
-                from: "users",
-                localField: "owner",
-                foreignField: "_id",
-                as: "owner"
-            }
-        },
-        {
-            $unwind: "$owner"
-        },
-        {
-            $project: {
-                "owner.password": 0,
-                "owner.email": 0
-            }
-        },
-        Comment.find({video: videoId}),
-        {page, limit}
-    )
-
-    return res.status(200).json(new ApiResponse(200, {videoId, comments}, "All comments get"))
+   try {
+     //TODO: get all comments for a video
+     const {videoId} = req.params
+     const {page = 1, limit = 10} = req.query
+     if(!videoId || !mongoose.isValidObjectId(videoId)){
+         throw new ApiError(400, "Video Id is incorrect to get all comments")
+     }
+ 
+     const comments = await Comment.aggregatePaginate(
+         
+         {
+             $match: {
+                 video: mongoose.Types.ObjectId(videoId)
+             }
+         },
+         {
+             page,
+             limit
+         },
+         {
+             $lookup: {
+                 from: "users",
+                 localField: "owner",
+                 foreignField: "_id",
+                 as: "owner"
+             }
+         },
+         {
+             $unwind: "$owner"
+         },
+         {
+             $project: {
+                 "owner.password": 0,
+                 "owner.email": 0
+             }
+         },
+         Comment.find({video: videoId}),
+         {page, limit}
+     )
+ 
+     return res.status(200).json(new ApiResponse(200, {videoId, comments}, "All comments get"))
+   } catch (error) {
+     throw new ApiError(400, error?.message || "Some error in getVideoComments")
+    
+   }
 
 })
 
