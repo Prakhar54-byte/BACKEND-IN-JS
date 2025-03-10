70c959e317b9cd5e5967ffa09ed8576ec71f0c05
Prakhar54-byte <b23bb1032@iitj.ac.in>
2025-03-11 00:52:11 +0530

    Updated backend files

--- src/index.js
+++ src/index.js
@@ -23,7 +23,8 @@ connectDB()
 
     app.listen(process.env.PORT || 8000,()=>{
         console.log(`SERVER is running at port ${process.env.PORT}`);
-        
+        console.log(`MONGO DB IS CONNECTED TO ${DB_NAME}`);
+            
     })
 })
 .catch((e)=>{
