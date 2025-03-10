000664ec4f70771341e149a25b8f8f96bf2255c0
Prakhar54-byte <b23bb1032@iitj.ac.in>
2025-03-10 18:51:03 +0530

    test commit

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
         const connectionInstance = await mongoose.connect(`${process.env.MONOGODB_URI}/${DB_NAME}`);
         // console.log("Thsi si connectionInstance",connectionInstance);
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-        
+        connectDB();
+        // console.log("Thsi si connectionInstance",connectionInstance);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
         process.exit(1);
