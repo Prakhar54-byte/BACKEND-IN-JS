b2c06cd3f6aa9c72ac9431e3e79241cf233efc8b
Prakhar54-byte <b23bb1032@iitj.ac.in>
2025-03-23 10:07:20 +0530

    Update MongoDB and Cloudinary versions; improve database connection handling and error logging

--- src/db/index.js
+++ src/db/index.js
@@ -1,20 +1,21 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        // console.log("This is connectionInstance",connectionInstance);
+        const dbUrl = process.env.MONGODB_URL;
+        
+        if (!dbUrl) {
+            console.error("MONGODB_URL is not defined in environment variables");
+            process.exit(1);
+        }
+        
+        const connectionInstance = await mongoose.connect(dbUrl);
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-        connectDB();
-        // debugger;
-
     } catch (error) {
         console.log("MONGODB connection FAILED !!! index", error);
         process.exit(1);
-        
     }
-    
 }
 
 
