df1b0b7dda65aca8c27ca16f01966c2ffd96a596
Prakhar54-byte <162185166+Prakhar54-byte@users.noreply.github.com>
2025-05-22 17:06:30 +0530

    Refactor codebase for improved structure and readability; add ping route, adjust body size limit handling, and update user routes; enhance error handling in ApiError and ApiResponse classes; clean up console logs and remove unused code.

--- src/db/index.js
+++ src/db/index.js
@@ -1,40 +1,10 @@
 import mongoose from "mongoose";
-import {DB_NAME} from "../constants.js";
+import { DB_NAME } from "../constants.js";
+
 
 const connectDB = async () => {
     try {
-        const dbUrl = process.env.MONGODB_URL;
-        
-        if (!dbUrl) {
-            console.error("MONGODB_URL is not defined in environment variables");
-            process.exit(1);
-        }
-        
-        const connectionInstance = await mongoose.connect(dbUrl);
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log("MONGODB connection FAILED !!! index", error);
-        process.exit(1);
-    }
-}
-
-
-export default connectDB;
-
-
-
-
-
-
-
-
-/*
-import express from "express";
-const app = express();
-
-const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
@@ -43,5 +13,3 @@ const connectDB = async () => {
 }
 
 export default connectDB
-
-*/
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,15 +2,17 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+      cb(null, "./public/uploads")
+      console.log("File destination: ", file);
+      
     },
     filename: function (req, file, cb) {
       
       cb(null, file.originalname)
+      console.log("File name: ", file.originalname);
     }
   })
   
-  
 export const upload = multer({ 
-    storage,  limits: { fileSize: 5 * 1024 * 1024 }, 
+    storage, 
 })
