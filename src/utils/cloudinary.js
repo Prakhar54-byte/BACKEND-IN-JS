6016e4c6b535e73c4cad8b8bf456f59f616f86ca
Prakhar54-byte <162185166+Prakhar54-byte@users.noreply.github.com>
2025-06-14 20:08:25 +0530

    Add logging for Cloudinary configuration with environment variables

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -48,7 +48,9 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
+console.log("Configuring Cloudinary with environment variables",process.env);
 cloudinary.config({ 
+  
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
   api_secret: process.env.CLOUDINARY_API_SECRET ,
