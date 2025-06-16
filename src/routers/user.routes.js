c67b6012b4d1b649f451450058419a626de4792e
Prakhar54-byte <162185166+Prakhar54-byte@users.noreply.github.com>
2025-06-17 00:14:00 +0530

    Enhance CORS configuration, improve user login handling, and add secure cookie options; update user routes for better readability

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -40,7 +40,7 @@ router.route("/login").post(logInUser)
 router.route("/logout").post(verifyJWT,  loggedOut)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
-router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/current-user").get( verifyJWT,getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
